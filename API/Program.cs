267bbb308c3c1844fd51848537a2828332675338
Duke <duch.giang@gmail.com>
2023-12-26 16:51:35 -0500

    testing fallback disabled

--- API/Program.cs
+++ API/Program.cs
@@ -41,7 +41,7 @@ app.UseStaticFiles();
 
 app.MapControllers();
 app.MapHub<ChatHub>("/chat");
-app.MapFallbackToController("Index", "Fallback");
+// app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateAsyncScope();
 var services = scope.ServiceProvider;
